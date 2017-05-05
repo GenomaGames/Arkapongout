3723d8c23e56f9a16b88c876a64caeeff4a60f12
Alberto Fernandez <albertofdzm@gmail.com>
2017-05-06 01:40:59 +0200

    Add Main Menu. close #6

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,16 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
 
+	public static GameController instance = null;
+
 	ScoreController scoreCtrl;
+	SceneController sceneCtrl;
 
 	void Awake () {
+		Init();
+
 		scoreCtrl = GetComponent<ScoreController>();
+		sceneCtrl = GetComponent<SceneController>();
+		SceneManager.sceneLoaded += OnSceneLoaded;
+	}
+
+	public void StartGame () {
+		sceneCtrl.LoadNext();
+	}
+
+	void Init () {
+		if (instance == null) {
+			instance = this;
+		} else {
+			Destroy(gameObject);
+		}
+
+		DontDestroyOnLoad(gameObject);
 	}
 
 	void OnScore (int score) {
-		scoreCtrl.AddScore(score);
+		scoreCtrl.Add(score);
+	}
+
+	void OnSceneLoaded (Scene scene, LoadSceneMode mode) {
+		if (scene.name.Contains("Game")) {
+			scoreCtrl.Init();
+		}
 	}
 }
--- Assets/Scripts/ScoreController.cs
+++ Assets/Scripts/ScoreController.cs
@@ -6,22 +6,36 @@ public class ScoreController : MonoBehaviour {
 	public Text scoreText;
 	public int score = 0;
 
-	// Use this for initialization
 	void Start () {
+		if (GameObject.FindWithTag("Score") != null) {
+			scoreText = GameObject.FindWithTag("Score").GetComponent<Text>();
+		}
+
 		UpdateUI();
 	}
 
-	public void SetScore (int newScore) {
+	public void Init () {
+		if (GameObject.FindWithTag("Score") != null) {
+			scoreText = GameObject.FindWithTag("Score").GetComponent<Text>();
+		}
+
+		score = 0;
+		UpdateUI();
+	}
+
+	public void Set (int newScore) {
 		score = newScore;
 		UpdateUI();
 	}
 
-	public void AddScore (int pScore) {
-		score +=pScore;
+	public void Add (int pScore) {
+		score += pScore;
 		UpdateUI();
 	}
 
 	void UpdateUI () {
-		scoreText.text = score.ToString();
+		if (scoreText != null) {
+			scoreText.text = score.ToString("D6");
+		}
 	}
 }

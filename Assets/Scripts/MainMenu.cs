b0798315fe910978e37f750b176f28033fea3939
Alberto Fernandez <albertofdzm@gmail.com>
2017-05-06 23:59:55 +0200

    Version 0.0.0-p1

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,6 +1,13 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour {
+
+	public Text versionText;
+
+	void Start () {
+		versionText.text = 'v' + Application.version;
+	}
 
 	public void StartGame () {
 		SceneController.LoadNext();

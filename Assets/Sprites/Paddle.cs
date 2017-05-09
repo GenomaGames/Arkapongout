c0d296e2ec88ceaaaeef91085c9cb0ac7df861d4
Alberto Fernandez <albertofdzm@gmail.com>
2017-05-09 22:01:13 +0200

    Add autopilot option to paddle. Close #24

---
+++ Assets/Sprites/Paddle.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+	public bool autopilot = false;
+
+	GameObject ball;
+
+	void Awake () {
+		ball = GameObject.FindGameObjectWithTag("Ball");
+	}
+
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (autopilot) {
+			if (GetComponent<MouseFollower2D>() != null && GetComponent<MouseFollower2D>().enabled) {
+				GetComponent<MouseFollower2D>().enabled = false;
+			}
+
+			if (Mathf.Abs(ball.GetComponent<Rigidbody2D>().velocity.x) > .5f) {
+				transform.position = new Vector2(ball.transform.position.x, transform.position.y);
+			} else {
+				transform.position = new Vector2(ball.transform.position.x - .9f, transform.position.y);
+			}
+		} else if (GetComponent<MouseFollower2D>() != null && !GetComponent<MouseFollower2D>().enabled) {
+			GetComponent<MouseFollower2D>().enabled = true;
+		}
+	}
+}

f47f6aa267ad3771121af84420dd10293cb74c84
Alberto Fernandez <albertofdzm@gmail.com>
2017-05-17 21:19:54 +0200

    Fix link open in browsers

--- Assets/Scripts/LinkButton.cs
+++ Assets/Scripts/LinkButton.cs
@@ -3,6 +3,10 @@
 public class LinkButton : MonoBehaviour {
 
 	public void OpenUrl (string url) {
-		Application.OpenURL(url);
+		if(Application.platform == RuntimePlatform.WebGLPlayer) {
+			Application.ExternalEval("window.open(\"" + url + "\",\"_blank\")");
+		} else {
+			Application.OpenURL(url);
+		}
 	}
 }

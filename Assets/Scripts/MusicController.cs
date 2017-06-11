bd6cba5e956a1d301ada4ea4bca38f6a39bbc7e9
Alberto Fernandez <albertofdzm@gmail.com>
2017-06-11 18:28:28 +0200

    Add musci controller and game level music

---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicController : Singleton<MusicController> {
+
+	public AudioClip[] musicClips;
+
+	AudioSource audioSrc;
+
+	void Awake () {
+		DontDestroyOnLoad(gameObject);
+	}
+
+	void Start () {
+		SceneManager.sceneLoaded += OnSceneLoaded;
+		audioSrc = GetComponent<AudioSource>();
+		audioSrc.clip = musicClips[SceneManager.GetActiveScene().buildIndex];
+		audioSrc.Play();
+	}
+
+	void OnSceneLoaded (Scene scene, LoadSceneMode mode) {
+		audioSrc.clip = musicClips[scene.buildIndex];
+		audioSrc.Play();
+	}
+}
